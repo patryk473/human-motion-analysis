76ef1286b55675eb2d1ccfbcad06d404be3f19f2
Patryk Ciechanowski <cpatryk2@wp.pl>
2026-03-12 20:27:19 +0100

    usunięcie niepotrzebnych rzeczy

--- archiwum/esp32_imu/api_handlers.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <WebServer.h>
-
-void setupApi(WebServer &server);
--- archiwum/esp32_imu/imu_sensor.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Adafruit_LSM6DS3TRC.h>
-
-void imuInit();
-bool imuRead(
-  int id,
-  float &ax, float &ay, float &az,
-  float &gx, float &gy, float &gz
-);
